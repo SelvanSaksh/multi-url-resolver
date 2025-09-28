743b4d1b21d18e1f0bbc950cb6da74073cb19dc3
SelvanSaksh <muthamizh.selvan@sakksh.com>
2025-09-28 15:20:17 +0530

    sad

--- src/IdPage.jsx
+++ src/IdPage.jsx
@@ -627,7 +627,7 @@ const IdPage = () => {
 
                     await api.post(scanUrl, payload);
                     console.log("Scan details sent successfully");
-                    // window.location.replace(finalUrl);
+                    window.location.replace(finalUrl);
                 } else {
                     setShowDetails(true);
                 }
